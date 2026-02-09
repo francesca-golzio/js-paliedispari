c8048d3a200a913a2c0828a3d9303db61d018b84
Francesca Golzio <francescagolzio@gmail.com>
2026-02-09 15:06:59 +0100

    add and store const userWord

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,9 +1,17 @@
 /* is it working? */
 console.log("🟢 it's working...")
 
+/* 🪞 Palidroma */
 
+/* Chiedo all’utente di inserire una parola */
 
+  /* creo un prompt e lo salvo in una variabile */
+  const userWord = 'Anna'/* prompt('Type a word, please') */
+  //console.log(userWord);
 
+/* Creo una funzione, per capire se la parola inserita è palindroma */
+
+  /*  */
 
 
 
