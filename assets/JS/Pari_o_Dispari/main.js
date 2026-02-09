929128f52eb906c548b5c8fb871ff1414b924e00
Francesca Golzio <francescagolzio@gmail.com>
2026-02-09 17:33:52 +0100

    add ask user prompts

--- assets/JS/Pari_o_Dispari/main.js
+++ assets/JS/Pari_o_Dispari/main.js
@@ -3,4 +3,28 @@
 
 /* ✌️🫲 Pari o Dispari ? */
 
+  /* Chiedo all' Utente di rispondere a due prompt */
+    /* 1 - Scegli pari o dispari*/
+    const userChoice = prompt('Choose and type "true" or "false"');
+    console.log(userChoice);
+    
+    
+    /* 2 - Inserisci un numero da 1 a 5 */
+    const userNumb = prompt('Digit a number between 1 and 5');
+    console.log(userNumb);
+
+  /* Genero un numero random (1-5) per il computer (uso una funzione) */
+    
+    /* Raccolgo i valori */
+    
+
+    /* Scrivo la funzione */
+    
+
+    
+
+  /* Sommo i due numeri */
   
+  /* Stabilisco, con una funzione, se la somma dei due numeri è pari/dispari */
+
+  /* Dichiaro chi ha vinto */
