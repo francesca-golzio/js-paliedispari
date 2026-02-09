b03625542bd2c0335c2745f0535b95e258e2f99f
Francesca Golzio <francescagolzio@gmail.com>
2026-02-09 15:27:55 +0100

    add letters inverter

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,18 +1,29 @@
 /* is it working? */
-console.log("🟢 it's working...")
+//console.log("🟢 it's working...")
 
 /* 🪞 Palidroma */
 
-/* Chiedo all’utente di inserire una parola */
+  /* Chiedo all’utente di inserire una parola */
 
-  /* creo un prompt e lo salvo in una variabile */
-  const userWord = 'Anna'/* prompt('Type a word, please') */
-  //console.log(userWord);
+    /* creo un prompt e lo salvo in una variabile */
+    const userWord = 'Anna'/* prompt('Type a word, please') */
+    //console.log(userWord);
 
-/* Creo una funzione, per capire se la parola inserita è palindroma */
+  /* Creo una funzione, per capire se la parola inserita è palindroma */
+    
+    /* Creo un array vuoto in cui conservare le lettere nell'ordine inverso */  
+    let reversedLettersOrder = [];
 
-  /*  */
-
+    /* All'interno di un ciclo for()... */    
+    for(let i = 0; i < userWord.length; i++) {
+      /* prendo ogni lettera nell'ordine */
+      /* e la salvo in una variabile */
+      const character = userWord.charAt(i);
+      console.log(character);
+      /* aggiungo il valore (lettera) all'array creato prima */
+      reversedLettersOrder.unshift(character);
+      console.log(reversedLettersOrder);      
+    }
 
 
 
